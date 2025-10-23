e904f66b01fee8fe544060c8ef07eab394830ce3
ovidas00 <designerovi00@gmail.com>
2025-10-23 16:09:22 +0600

    updated

---
+++ src/app/dashboard/transactions/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Transactions() {
+  const { data: transData } = useQuery({
+    queryKey: ["transactions"],
+    queryFn: async () => {
+      const response = await api.get("/transactions");
+      return response.data.payload;
+    },
+  });
+
+  return (
+    <section className="p-4 md:p-6">
+      <div className="max-w-7xl bg-[#13171E] border border-[#2A2A2A] rounded-lg">
+        <h1 className="text-lg text-white font-medium px-[20px] py-[10px]">
+          Transactions
+        </h1>
+
+        <table className="w-full text-sm text-left text-gray-300 bg-[#03070D] divide-y divide-[#2A2A2A] rounded-lg">
+          <thead className="bg-[#25262A] text-gray-200">
+            <tr>
+              <th scope="col" className="px-4 py-3">
+                Type
+              </th>
+              <th scope="col" className="px-4 py-3 hidden sm:table-cell">
+                Date
+              </th>
+              <th scope="col" className="px-4 py-3 text-center">
+                Amount
+              </th>
+              <th scope="col" className="px-4 py-3 hidden sm:table-cell">
+                Currency
+              </th>
+              <th scope="col" className="px-4 py-3">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-[#2A2A2A]">
+            {(transData?.transactions || []).map((tx: any) => (
+              <tr key={tx.id}>
+                <td className="px-4 py-3">{tx.transactionType}</td>
+                <td className="px-4 py-3 hidden sm:table-cell">
+                  {new Date(tx.createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </td>
+                <td className="px-4 py-3 text-center">
+                  {tx.amount > 0 ? `+` : ""}
+                  {new Intl.NumberFormat("en-US").format(tx.amount)}
+                </td>
+                <td className="px-4 py-3 hidden sm:table-cell">
+                  {tx.currency}
+                </td>
+                <td className="px-4 py-3">
+                  {tx.status === "Completed" ? (
+                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-green-500/20 text-green-400">
+                      Completed
+                    </span>
+                  ) : (
+                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-yellow-500/20 text-yellow-400">
+                      Pending
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
